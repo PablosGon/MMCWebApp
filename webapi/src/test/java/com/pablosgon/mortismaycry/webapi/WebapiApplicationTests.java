f072c74f7c27595d0b9bcd26944ba08003a8d505
PablosGon <100768678+PablosGon@users.noreply.github.com>
2025-03-04 15:31:02 +0100

    setup projects

---
+++ webapi/src/test/java/com/pablosgon/mortismaycry/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablosgon.mortismaycry.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
