2d55167e38918c7a290c8b7b4c0c48f326a86a9f
PablosGon <100768678+PablosGon@users.noreply.github.com>
2025-04-23 20:05:02 +0200

    fixed accordion buttons

--- mmcapp/src/components/player/player-achievements.component.tsx
+++ mmcapp/src/components/player/player-achievements.component.tsx
@@ -15,24 +15,24 @@ export function PlayerAchievementsComponent(params: { badges: StarBadgeCase }) {
     return (
         <div className="flex flex-col w-full">
             <article>
-                <header className="flex flex-row items-center" onClick={() => toggle(1)}>
+                <button className="flex flex-row items-center w-full" onClick={() => toggle(1)}>
                     <Image src="/star_players/grand_star_player.png" width={50} height={50} alt="Gran estelar" />
                     <p>Gran estelar</p>
                     <p className="ml-auto">{badges.grandStarPlayerBadges.length}</p>
-                </header>
+                </button>
                 {
                     openIndex === 1 &&
                         <div>
                             <p className="text-sm">El título de Gran Estelar se otorga al jugador que más trofeos ha subido a lo largo de una temporada.</p>
-                            <ul className="flex flex-row flex-wrap justify-center flex-1">
+                            <ul className="flex flex-row flex-wrap justify-center flex-1 gap-2">
                                 {
                                     badges.grandStarPlayerBadges.map((badge, index) => (
                                         <li key={index} className="flex flex-col items-center flex-1">
                                             <Image src="/star_players/grand_star_player.png" height={100} width={100} alt="Gran estelar"/>
-                                            <p className="text-xl">
+                                            <p className="text-md">
                                                 Temporada {badge.seasonId}
                                             </p>
-                                            <p className="text-lg text-amber-400">
+                                            <p className="text-sm text-amber-400">
                                                 +{badge.trophies} trofeos
                                             </p> 
                                         </li>
@@ -44,16 +44,16 @@ export function PlayerAchievementsComponent(params: { badges: StarBadgeCase }) {
             </article>
             <hr/>
             <article>
-                <header className="flex flex-row items-center" onClick={() => toggle(2)}>
+                <button className="flex flex-row items-center w-full" onClick={() => toggle(2)}>
                     <Image src="/star_players/star_legend.png" width={50} height={50} alt="Leyenda estelar" />
                     <p>Leyenda estelar</p>
                     <p className="ml-auto">{badges.starLegendBadges.length}</p>
-                </header>
+                </button>
                 {
                     openIndex === 2 &&
                         <div>
                             <p className="text-sm">El título de Leyenda Estelar se otorga al jugador que más trofeos totales tenga acumulados al final de la temporada.</p>
-                            <ul className="flex flex-row flex-wrap justify-center flex-1">
+                            <ul className="flex flex-row flex-wrap justify-center flex-1 gap-2">
                                 {
                                     badges.starLegendBadges.map((badge, index) => (
                                         <li key={index} className="flex flex-col items-center flex-1">
@@ -73,16 +73,16 @@ export function PlayerAchievementsComponent(params: { badges: StarBadgeCase }) {
             </article>
             <hr/>
             <article>
-                <header className="flex flex-row items-center" onClick={() => toggle(3)}>
+                <button className="flex flex-row items-center w-full" onClick={() => toggle(3)}>
                     <Image src="/star_players/star_master.png" width={50} height={50} alt="Maestro estelar" />
                     <p>Maestro estelar</p>
                     <p className="ml-auto">{badges.starMasterBadges.length}</p>
-                </header>
+                </button>
                 {
                     openIndex === 3 &&
                         <div>
                             <p className="text-sm">El título de Maestro Estelar se otorga al jugador que más alto se encuentre en ranked al final de la temporada.</p>
-                            <ul className="flex flex-row flex-wrap justify-center flex-1">
+                            <ul className="flex flex-row flex-wrap justify-center flex-1 gap-2">
                                 {
                                     badges.starMasterBadges.map((badge, index) => (
                                         <li key={index} className="flex flex-col items-center flex-1">
@@ -102,11 +102,11 @@ export function PlayerAchievementsComponent(params: { badges: StarBadgeCase }) {
             </article>
             <hr/>
             <article>
-                <header className="flex flex-row items-center" onClick={() => toggle(4)}>
+                <button className="flex flex-row items-center w-full" onClick={() => toggle(4)}>
                     <Image src="/star_players/star_player.png" width={50} height={50} alt="Estelar" />
                     <p>Jugador estelar</p>
                     <p className="ml-auto">{badges.starPlayerBadges.length}</p>
-                </header>
+                </button>
                 {
                     openIndex === 4 &&
                         <div>
