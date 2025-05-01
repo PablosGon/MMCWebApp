221627274b7876d824fdd1409525c22111e46ce4
PablosGon <100768678+PablosGon@users.noreply.github.com>
2025-05-01 12:11:54 +0200

    resolved conflict

--- webapi/src/main/java/com/pablosgon/mortismaycry/webapi/repositories/TrophyRegistryRepository.java
+++ webapi/src/main/java/com/pablosgon/mortismaycry/webapi/repositories/TrophyRegistryRepository.java
@@ -18,7 +18,4 @@ public interface TrophyRegistryRepository extends JpaRepository<JPATrophyRegistr
     @Query("select r from JPATrophyRegistry r where r.season = :season and week = :week")
     List<JPATrophyRegistry> findRegistriedByWeek(int season, int week);
 
-    @Query("select r from JPATrophyRegistry r order by season asc, week asc")
-    List<JPATrophyRegistry> findAllSorted();
-
 }
