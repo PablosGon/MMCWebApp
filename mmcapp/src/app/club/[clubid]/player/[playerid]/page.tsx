f74461ee1d65a486db550396665cb85f026904b0
PablosGon <100768678+PablosGon@users.noreply.github.com>
2025-06-21 13:32:04 +0200

    remapped player page

--- mmcapp/src/app/club/[clubid]/player/[playerid]/page.tsx
+++ mmcapp/src/app/club/[clubid]/player/[playerid]/page.tsx
@@ -10,7 +10,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
 
 export default function PlayerPage() {
 
@@ -57,50 +56,22 @@ export default function PlayerPage() {
                     </div>
                 </header>
                 <div className="container flex flex-wrap gap-5">
-                    <section className="container bg-gray-800 p-10 rounded-4xl justify-items-center lg:justify-start mt-5 flex-1/4">
-                        <h2 className="text-2xl sm:text-2xl md:text-3xl text-center">Progreso de temporada</h2>
+                    <section className="container bg-gray-800 p-10 rounded-4xl justify-items-center lg:justify-start mt-5">
+                        <h2 className="text-2xl sm:text-2xl md:text-3xl text-center">Progreso de trofeos</h2>
                         <TrophyChart player={player}/>
                     </section>
                     <section className="container bg-gray-800 p-10 rounded-4xl justify-items-center lg:justify-start mt-5 flex-1/4">
                         <h2 className="text-2xl sm:text-2xl md:text-3xl text-center">Logros</h2>
                         <PlayerAchievementsComponent badges={player.badges} />
                     </section>
-                    <section className="container bg-gray-800 p-10 rounded-4xl justify-items-center lg:justify-start mt-5">
-                        <h2 className="text-2xl sm:text-2xl md:text-3xl text-center">Registro de megahucha</h2>
-                        <MegapigRegistriesComponent player={player} />
-                    </section>
-                    <section className="container bg-gray-800 p-10 rounded-4xl justify-items-center mt-5">
-                        <h2 className="text-2xl sm:text-2xl md:text-3xl text-center">Progreso total</h2>
-                        <Line data={{
-                            labels: player.trophyRegistries.map(tr => `T${tr.season} S${tr.week}`),
-                            datasets: [
-                                {
-                                    label: 'Trophies',
-                                    data: player.trophyRegistries.map(tr => tr.trophies),
-                                    borderColor: '#fcc200',
-                                    backgroundColor: '#fcc200',
-                                    fill: true,
-                                    animation: false,
-                                    borderWidth: 7,
-                                }
-                            ],
-                        }} options={{
-                            responsive: true,
-                            scales: {
-                                x: {
-                                },
-                                y: {
-                                    min: Math.min(...player.trophyRegistries.map(tr => tr.trophies)),
-                                    max: Math.max(...player.trophyRegistries.map(tr => tr.trophies))
-                                }
-                            },
-                            plugins: {
-                                legend: {
-                                    display: false
-                                }
-                            }
-                        }}/>
-                    </section>
+                    {
+                        sessionStorage.getItem('admin') &&
+                            <section className="container bg-gray-800 p-10 rounded-4xl justify-items-center lg:justify-start mt-5 flex-1/4">
+                                <h2 className="text-2xl sm:text-2xl md:text-3xl text-center">Registro de megahucha</h2>
+                                <MegapigRegistriesComponent player={player} />
+                            </section>
+                    }
+
                 </div>
                 
             </div>
--- mmcapp/src/components/player/trophy-chart.component.tsx
+++ mmcapp/src/components/player/trophy-chart.component.tsx
@@ -1,5 +1,7 @@
+import { TrophyChartMode } from "@/enums/trophy-chart-mode.enum";
 import { Player } from "@/models/player.model";
 import { Chart, LogarithmicScale, CategoryScale, Legend, LinearScale, LineElement, PointElement, Title, Tooltip } from "chart.js";
+import { useState } from "react";
 import { Line } from "react-chartjs-2";
 
 export function TrophyChart(params:Readonly<{player:Player}>) {
@@ -20,9 +22,7 @@ export function TrophyChart(params:Readonly<{player:Player}>) {
     const trophyUpgrades = player.seasonTrophyProgress;
     const trophyRegistries = player.trophyRegistries.filter(x => x.week != 0);
 
-    console.log("Trophy Registries:", player.trophyRegistries.filter(x => x.week != 0))
-
-    console.log([0, ...trophyUpgrades], trophyUpgrades)
+    const [mode, setMode] = useState<TrophyChartMode>(TrophyChartMode.Season);
 
     if(trophyUpgrades.length == 0) {
         return (
@@ -39,51 +39,93 @@ export function TrophyChart(params:Readonly<{player:Player}>) {
         )
     }
     return (
-        <div className={`max-w-100`}>
-            <Line data={{
-                labels: [0, ...trophyUpgrades],
-                datasets: [
-                    {
-                        label: 'Trophies',
-                        data: [0, ...trophyUpgrades],
-                        borderColor: '#fcc200',
-                        backgroundColor: '#fcc200',
-                        fill: true,
-                        animation: false,
-                        borderWidth: 7,
-                    }
-                ],
-            }} options={{
-                responsive: true,
-                scales: {
-                    x: {
-                        display: false
-                    },
-                    y: {
-                        display: false,
-                        min: -200,
-                        max: Math.max(...trophyUpgrades, 1200 + 200)
-                    }
-                },
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                }
-            }}/>
-            <div className="flex">
-                <div className="flex-1 justify-items-center">
-                    <p className="text-md sm:text-lg md:text-xl">Inicio</p>
-                </div>
-                {
-                    trophyUpgrades.map((tu, index) => (
-                        <div key={"tu"+index} className="flex-1 justify-items-center">
-                            <p className="text-md sm:text-lg md:text-xl">Sem {index+1}</p>
-                            <p className="text-amber-400">+{tu}</p>
-                        </div>
-                    ))
-                }
-            </div>
+        <div className="w-full">
+            <form className="max-w-sm w-full flex flex-row border-2 border-gray-600 rounded-2xl m-auto">
+                <label className="flex-1 text-center cursor-pointer">
+                    <input 
+                        type="radio" 
+                        name="mode"
+                        value={TrophyChartMode.Season} 
+                        onChange={() => setMode(TrophyChartMode.Season)} 
+                        className="peer hidden"
+                    />
+                    <span className="block p-2 peer-checked:bg-gray-600 overflow-hidden rounded-l-xl">Temporada</span>
+                </label>
+                <label className="flex-1 text-center cursor-pointer">
+                    <input 
+                        type="radio" 
+                        name="mode"
+                        value={TrophyChartMode.History} 
+                        onChange={() => setMode(TrophyChartMode.History)} 
+                        className="peer hidden"
+                    />
+                    <span className="block p-2 peer-checked:bg-gray-600 overflow-hidden rounded-r-2xl">Histórico</span>
+                </label>
+            </form>
+
+            {
+                mode === TrophyChartMode.History ?
+                    <Line data={{
+                        labels: player.trophyRegistries.map(tr => `T${tr.season} S${tr.week}`),
+                        datasets: [
+                            {
+                                label: 'Trophies',
+                                data: player.trophyRegistries.map(tr => tr.trophies),
+                                borderColor: '#fcc200',
+                                backgroundColor: '#fcc200',
+                                fill: true,
+                                animation: false,
+                                borderWidth: 7,
+                            }
+                        ],
+                    }} options={{
+                        responsive: true,
+                        scales: {
+                            x: {
+                            },
+                            y: {
+                                min: Math.min(...player.trophyRegistries.map(tr => tr.trophies)),
+                                max: Math.max(...player.trophyRegistries.map(tr => tr.trophies))
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: false
+                            }
+                        }
+                    }}/>
+                :
+                    <Line data={{
+                        labels: [0, ...trophyUpgrades].map((tr, week) => week == 0 ? 'Inicio' : `Semana ${week}`),
+                        datasets: [
+                            {
+                                label: 'Trophies',
+                                data: [0, ...trophyUpgrades],
+                                borderColor: '#fcc200',
+                                backgroundColor: '#fcc200',
+                                fill: true,
+                                animation: false,
+                                borderWidth: 7,
+                            }
+                        ],
+                    }} options={{
+                        responsive: true,
+                        scales: {
+                            x: {
+                            },
+                            y: {
+                                min: 0,
+                                max: Math.max(...trophyUpgrades, 2000)
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: false
+                            }
+                        }
+                    }}/>
+            }
+            
         </div>
     )
 }
